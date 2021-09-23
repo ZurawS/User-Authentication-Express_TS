69bcf58a7b295336c873803b680127160bb3b71e
PawZet <pawelzurawski898@gmail.com>
2021-09-23 15:41:02 +0200

    commit_6

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send("Not permitted");
+}
 var router = (0, express_1.Router)();
 exports.router = router;
 router.get("/login", function (req, res) {
@@ -9,5 +17,26 @@ router.get("/login", function (req, res) {
 });
 router.post("/login", function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    res.send(email + password);
+    if (email && password && email === "asd" && password === "qwe") {
+        req.session = { loggedIn: true };
+        res.redirect("/");
+    }
+    else {
+        res.send("\n    <div>\n    <div>You must provide correct email and password</div>\n    <div>\n        <a href='/login'>Login</a>\n    </div>\n    </div>\n    ");
+    }
+});
+router.get("/", function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n        <div>\n            <div>\n                You are logged in\n            </div>\n            <a href='/logout'>Logout</a>\n        </div>\n    ");
+    }
+    else {
+        res.send("\n    <div>\n        <div>\n            You are not logged in\n        </div>\n        <a href='/login'>Login</a>\n    </div>\n");
+    }
+});
+router.get("/logout", function (req, res) {
+    req.session = undefined;
+    res.redirect("/");
+});
+router.get("/protected", requireAuth, function (req, res) {
+    res.send("Welcome to the protected route, logged in user");
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,4 +1,17 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  res.status(403);
+  res.send("Not permitted");
+}
 
 const router = Router();
 
@@ -20,9 +33,53 @@ router.get("/login", (req: Request, res: Response) => {
   `);
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  res.send(email + password);
+
+  if (email && password && email === "asd" && password === "qwe") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
+  } else {
+    res.send(`
+    <div>
+    <div>You must provide correct email and password</div>
+    <div>
+        <a href='/login'>Login</a>
+    </div>
+    </div>
+    `);
+  }
+});
+
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+        <div>
+            <div>
+                You are logged in
+            </div>
+            <a href='/logout'>Logout</a>
+        </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+        <div>
+            You are not logged in
+        </div>
+        <a href='/login'>Login</a>
+    </div>
+`);
+  }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to the protected route, logged in user");
 });
 
 export { router };
